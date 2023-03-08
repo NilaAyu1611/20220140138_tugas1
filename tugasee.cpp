fa7a13c3e2423fdc59b863592ab46a3a8adb4970
NilaAyu1611 <nilaayucw@gmail.com>
2023-03-08 23:38:19 +0700

    first commit

---
+++ tugasee.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // begin
+    // numeric Luas, a, t
+    double Luas, a, t;
+    // display 'masukan alas = '
+    cout << "masukan alas = ";
+    // accept = a;
+    cin >> a;
+    // display 'masukan tinggi = '
+    cout << "masukan tinggi = ";
+    // accept = t;
+    cin >> t;
+    // compute Luas = a*t/2
+    Luas = a*t/2;
+    // display "Luasnya = "+Luas
+    cout << "Luasnya = " << Luas;
+    // end
+}
